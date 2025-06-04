3e38984ea00382f7028d36218af0bce8b1d187e7
Pratyush <choudharypratyush809@gmail.com>
2025-06-04 19:38:13 +0530

    update

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import {nanoid} from "nanoid";
 import connectDB from "./src/db/index.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 
 const app = express();
 dotenv.config({
     path: "./.env"
 });
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
--- BACKEND/src/controllers/shorturl.controller.js
+++ BACKEND/src/controllers/shorturl.controller.js
@@ -1,17 +1,20 @@
-import { nanoid } from "nanoid";
 import { asyncHandler } from "../utils/asynchandler.js";
 import { shortUrl } from "../models/shorturl.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
+import { generateNanoId } from "../services/helper.js";
 
 
 const createShortUrl = asyncHandler( async (req, res) => {
-    const { url } = req.body;
-    const short_url = nanoid(7);
+    const { url, userId } = req.body;
+    const short_url = await generateNanoId(7);
+
     if(!url){
         throw new ApiError(400, "url not found")
     }
-
+    if(!short_url){
+        throw new ApiError(400, "short url not generated")
+    }
     const newUrl = await shortUrl.create({
         full_url: url,
         short_url
@@ -19,15 +22,29 @@ const createShortUrl = asyncHandler( async (req, res) => {
     if(!newUrl){
         throw new ApiError(400, "newurl not created")
     }
+    if(userId){
+        newUrl.user._id = userId
+    }
+    newUrl.save();
+    const fullShortUrl = `${process.env.API_URL}/${newUrl.short_url}`
 
-    return res.status(200).json(new ApiResponse(200, newUrl, "short url is created successfully."))
+    return res.status(200).json(new ApiResponse(200, fullShortUrl , "short url is created successfully."))
 })
 
 const redirectShortUrl = asyncHandler( async (req, res) =>{
     const { shorturl } = req.params;
-    const url = await shortUrl.findOne({
-        short_url: shorturl
-    })
+    if(!shorturl){
+        throw new ApiError(400, "shorturl not fetched")
+    }
+    const url = await shortUrl.findOneAndUpdate(
+        { short_url: shorturl },
+        {
+            $inc: {
+                clicks: 1,
+            }
+        },
+        {new: true},
+    );
     console.log(url.full_url);
 
     if(!url){
@@ -35,6 +52,8 @@ const redirectShortUrl = asyncHandler( async (req, res) =>{
     }
     return res.redirect(url.full_url)
 })
+
+
 
 export { 
     createShortUrl,
