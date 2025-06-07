7c2f571719114bc51bd96fe5f545158f9f89806a
Pratyush <choudharypratyush809@gmail.com>
2025-06-08 02:37:15 +0530

    changes

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -10,7 +10,10 @@ dotenv.config({
     path: "./.env"
 });
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://url-shortner-frontend.vercel.app'],
+    origin: [
+        'http://localhost:5173',
+        'https://ur-lshortner-six.vercel.app'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
