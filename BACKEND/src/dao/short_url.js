36435409e80f08722b9beecf0d2cd7880f9ebb94
Pratyush <choudharypratyush809@gmail.com>
2025-06-07 23:54:34 +0530

    updated

--- BACKEND/src/dao/short_url.js
+++ BACKEND/src/dao/short_url.js
@@ -1,5 +1,5 @@
 import { shortUrl } from "../models/shorturl.model.js";
-import { asyncHandler } from "../utils/asynchandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const checkCustomUrl = asyncHandler ( async (slug) => {
     return await shortUrl.findOne({short_url: slug})
