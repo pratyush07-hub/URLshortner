b81f3996e8645774566000bafce9d1c6882a08d4
Pratyush <choudharypratyush809@gmail.com>
2025-06-08 01:59:57 +0530

    changes for vercel

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 5173,
+  }
 });
