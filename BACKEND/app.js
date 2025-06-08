4aac144b5d6ceed6fda1a667e94fb7ceb4350c50
Pratyush <choudharypratyush809@gmail.com>
2025-06-08 11:57:44 +0530

    small issue

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -12,7 +12,7 @@ dotenv.config({
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://url-shortner-frontend-coral.vercel.app/'
+        'https://url-shortner-frontend-coral.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
